db25e0d296eeb3ceae1b2e36200f5eb3fff3959b
Jogi123 <testmail@test.com>
2021-06-07 21:26:15 +0200

    added shutters.db

--- Rollladensteuerung/models.py
+++ Rollladensteuerung/models.py
@@ -7,9 +7,6 @@ class Shutter(db.Model):
     ip = db.Column(db.String, unique=True, nullable=False)
     state = db.Column(db.String, nullable=False)
 
-    def __repr__(self):
-        return f'{self.id}:{self.room}:{self.ip}:{self.state}'
-
 
 if __name__ == '__main__':
     db.create_all()
