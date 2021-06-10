8004d68101f6c5d1a1dbc41525f2f6f2c91c3d52
Jogi123 <testmail@test.com>
2021-06-10 11:44:01 +0200

    added functionality for adding and deleting shutters

--- Rollladensteuerung/routes.py
+++ Rollladensteuerung/routes.py
@@ -1,34 +1,44 @@
-from threading import Thread
-import configparser
-from flask import render_template, request, redirect, copy_current_request_context
+from flask import render_template, request, redirect
 from Rollladensteuerung import app, db
+from Rollladensteuerung.models import Shutter
 from Rollladensteuerung.communication import Client
 
 
 @app.route('/')
 def home():
-    config = configparser.ConfigParser()
-    config.read('shutters.ini')
-    shutters = []
-    for i in config.sections():
-        shutters.append(dict(config.items(i)))
-    return render_template('home.html', shutters=shutters)
+    return render_template('home.html', shutters=Shutter.query.all())
 
 
 @app.route('/handle_shutter_change', methods=['POST'])
 def handle_shutter_change():
     # call client class to send data to shutter control server in a new thread
     # sends data "open" or "close" to specified ip on port 80
+    # Client(request.form['ip'], request.form['state'])
 
-    @copy_current_request_context
-    def send_data():
-        Client(request.form['ip'], request.form['change'])
-
-    t1 = Thread(target=send_data)
-    t1.start()
-
-    shutter = (request.form['room'], request.form['change'], request.form['ip'])  # pass tuple to db
-    db.session.add(shutter)
+    update = Shutter.query.filter_by(room=request.form['room']).first()
+    update.state = request.form['state']
     db.session.commit()
 
     return redirect('/')
+
+
+@app.route('/add_shutter', methods=['GET', 'POST'])
+def add_shutter():
+    if request.method == 'GET':
+        return render_template('add_shutter.html')
+    elif request.method == 'POST':
+        shutter = Shutter(room=request.form['room'], ip=request.form['ip'], state=request.form['state'])
+        db.session.add(shutter)
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/delete_shutter', methods=['GET', 'POST'])
+def delete_shutter():
+    if request.method == 'GET':
+        return render_template('delete_shutter.html', shutters=Shutter.query.all())
+    elif request.method == 'POST':
+        for room in request.form:
+            Shutter.query.filter_by(room=room).delete()
+            db.session.commit()
+        return redirect('/')
