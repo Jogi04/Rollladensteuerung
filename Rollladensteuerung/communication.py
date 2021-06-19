12d5b501f03d5a873034c256e0e84698a56ff470
Jogi123 <testmail@test.com>
2021-06-19 11:45:53 +0200

    fixed communication.py

--- Rollladensteuerung/communication.py
+++ Rollladensteuerung/communication.py
@@ -3,18 +3,20 @@ import socket
 
 
 class Client:
-    def __init__(self, server_ip, send_data):
+    def __init__(self, server_ip, send_data, server_port=80):
         self.send_data = send_data
         self.server_ip = server_ip
+        self.server_port = server_port
         if self.server_is_reachable():
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect(self.server_ip)
+            self.client_socket.connect((self.server_ip, self.server_port))
             self.client_socket.send(self.send_data.encode())
         else:
             return
 
     def server_is_reachable(self):
-        if os.system(f'ping -c 1 {self.server_ip}') is 0:
+        exit_code = os.system(f'ping -c 2 {self.server_ip}')
+        if exit_code == 0:
             return True
         else:
             return False
